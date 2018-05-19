daa071051fd71d4f95e25fcd9f44e6092afa4685
maciej <fcwilczynski@gmail.com>
2018-05-19 10:15:16 -0700

    Add oshop

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD79HJHfSclFe-vpaKbKOKPHfLJZaw-8-k",
+    authDomain: "oshop-657e1.firebaseapp.com",
+    databaseURL: "https://oshop-657e1.firebaseio.com",
+    projectId: "oshop-657e1",
+    storageBucket: "",
+    messagingSenderId: "199009572745"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD79HJHfSclFe-vpaKbKOKPHfLJZaw-8-k",
+    authDomain: "oshop-657e1.firebaseapp.com",
+    databaseURL: "https://oshop-657e1.firebaseio.com",
+    projectId: "oshop-657e1",
+    storageBucket: "",
+    messagingSenderId: "199009572745"
+  }
 };
